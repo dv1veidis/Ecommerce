66a0fe89a87533e105ddbe60cf041de16aa36d5c
Deividas Gelžinis <limessi61@gmail.com>
2022-10-27 12:26:28 +0300

    1 commit

---
+++ EcommerceProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EcommerceProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceProject.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+               
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
